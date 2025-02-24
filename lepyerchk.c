7f6adff62ac1b7fe3c9f954f5cad9c67c2927102
Tankist256 <mrmarmuk69@gmail.com>
2025-02-24 15:43:04 +0200

    main

---
+++ lepyerchk.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void leapyearchk(int year){
+if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+printf("%d is a leap year\n",year);
+}else{
+printf("%d is not a leap year\n",year);}
+}
+int main(){
+int year;
+printf("enter a year:");
+scanf("%d", &year);
+leapyearchk(year);
+return 0;
+}
